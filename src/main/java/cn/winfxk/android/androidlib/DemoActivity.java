2200e3513d0e10d62b9538810c3c4b63d36a3dd4
unknown <kc3115@kingclean.com>
2022-06-11 20:02:55 +0800

    Update alpha

---
+++ src/main/java/cn/winfxk/android/androidlib/DemoActivity.java
@@ -0,0 +1,13 @@
+package cn.winfxk.android.androidlib;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class DemoActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ src/main/java/cn/winfxk/android/androidlib/ViewData.java
@@ -0,0 +1,11 @@
+package cn.winfxk.android.androidlib;
+
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ViewData {
+    public ImageView imageView1;
+    public TextView textView1, textView2;
+    public Object Tag;
+}
